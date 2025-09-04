60d3665b0b0549eb02b43cc9008220935e80466c
LeonardoAvilaa <leonardo.avila.paranhos@gmail.com>
2025-09-04 20:43:53 -0300

    praticas aula4

---
+++ aula4/praticajs/praticajs.js
@@ -0,0 +1,21 @@
+    function login() { 
+        let user = document.getElementById("user").value;
+        let senha = document.getElementById("senha").value;
+        let senhaInput = document.getElementById("senha");
+        let userInput = document.getElementById("user");
+        if (user === "user" && senha === "pass") {
+            userInput.classList.remove("user_pass_fail"); 
+            senhaInput.classList.remove("user_pass_fail");
+            alert("Login OK");
+        }
+        else {
+            userInput.classList.add("user_pass_fail"); 
+            senhaInput.classList.add("user_pass_fail"); 
+        }
+        console.log ("aaaa");
+    };
+
+    var entrar = document.getElementById("entrar");
+    entrar.addEventListener("click", login);
+    
+
