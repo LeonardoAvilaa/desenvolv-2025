2c7ee7c9aaac04ee3fe2fb7f0bfcb314e3569305
LeonardoAvilaa <leonardo.avila.paranhos@gmail.com>
2025-09-25 20:21:42 -0300

    pratica 2 aula 8

--- aula8/pratica2.php
+++ aula8/pratica2.php
@@ -1,12 +1,18 @@
 <?php
 $salario1 = 1000;
-$salario2 = 2000; 
+$salario2 = 2000;
 
 $salario2 = $salario1;
-
 ++$salario2;
-
 $salario1 *= 1.1;
 
 echo "Salario 1: $salario1, Salario 2: $salario2";
+
+if ($salario1 > $salario2) {
+   echo "O Valor da variável 1 é maior que o valor da variável 2";
+} else if ($salario1 < $salario2) {
+   echo "O Valor da variável 2 é maior que o valor da variável 1";
+} else {
+   echo "Os valores são iguais";
+}
 ?>
